c560b3853a6f76c4d34d3760ce1cb96be0037970
Adam Tulloss <artulloss@gmail.com>
2024-11-28 19:46:57 -0500

    Spritesheet loads

--- src/game/scenes/Preloader.ts
+++ src/game/scenes/Preloader.ts
@@ -1,47 +1,43 @@
-import { Scene } from 'phaser';
+import { Scene } from "phaser";
 
-export class Preloader extends Scene
-{
-    constructor ()
-    {
-        super('Preloader');
-    }
+export class Preloader extends Scene {
+  constructor() {
+    super("Preloader");
+  }
 
-    init ()
-    {
-        //  We loaded this image in our Boot Scene, so we can display it here
-        this.add.image(512, 384, 'background');
+  init() {
+    // Display the background image
+    this.add.image(512, 384, "background");
 
-        //  A simple progress bar. This is the outline of the bar.
-        this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
+    // Create the outline of the progress bar
+    this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
 
-        //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
-        const bar = this.add.rectangle(512-230, 384, 4, 28, 0xffffff);
+    // Create the progress bar itself
+    const bar = this.add.rectangle(512 - 230, 384, 4, 28, 0xffffff);
 
-        //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
-        this.load.on('progress', (progress: number) => {
+    // Update the progress bar based on the loading progress
+    this.load.on("progress", (progress: number) => {
+      bar.width = 4 + 460 * progress;
+    });
+  }
 
-            //  Update the progress bar (our bar is 464px wide, so 100% = 464px)
-            bar.width = 4 + (460 * progress);
+  preload() {
+    // Set the base path for assets
+    this.load.setPath("assets");
 
-        });
-    }
+    // Load individual images
+    this.load.image("logo", "logo.png");
+    this.load.image("star", "star.png");
 
-    preload ()
-    {
-        //  Load the assets for the game - Replace with your own assets
-        this.load.setPath('assets');
+    // Load the texture atlas using the XML file
+    this.load.atlasXML("sprites", "spritesheet.png", "spritesheet.xml");
+  }
 
-        this.load.image('logo', 'logo.png');
-        this.load.image('star', 'star.png');
-    }
+  create() {
+    // Optional: Log all loaded textures to the console for debugging
+    console.log(this.textures.list);
 
-    create ()
-    {
-        //  When all the assets have loaded, it's often worth creating global objects here that the rest of the game can use.
-        //  For example, you can define global animations here, so we can use them in other scenes.
-
-        //  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
-        this.scene.start('MainMenu');
-    }
+    // Start the MainMenu scene once loading is complete
+    this.scene.start("MainMenu");
+  }
 }

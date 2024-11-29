20d7ac1ca86634d67d56b15cf4415901e93b3768
Adam Tulloss <artulloss@gmail.com>
2024-11-28 22:07:14 -0500

    Attempt 1 for setting up GH pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,20 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter({
-			precompress: false,
-			fallback: 'index.html'
-		})
-	}
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      precompress: false,
+      fallback: "index.html",
+    }),
+    paths: {
+      base: process.env.NODE_ENV === "production" ? "/final-project-template-artulloss/" : "",
+    },
+  },
 };
 
 export default config;
